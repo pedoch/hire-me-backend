c2a89bf54dd64981c6e961deca4fcc9417e1a3a7
PedroOgheneochuko <pedroogheneochuko@gmail.com>
2020-08-14 19:23:09 +0100

    Add secondary resume for Post Response model

--- models/Response.js
+++ models/Response.js
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ResponseSchema = new mongoose.Schema({
-	userId: {
-		type: Schema.Types.ObjectId,
-		ref: 'user',
-		required: true,
-	},
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  alternativeResume: {
+    type: String,
+  },
 });
 
-module.exports = Response = mongoose.model('response', ResponseSchema);
+module.exports = Response = mongoose.model("response", ResponseSchema);
