0c57555c8dada310b32509cab7c9d0f3be3b2dc9
Deltanboi <pedroogheneochuko@gmail.com>
2021-01-25 13:28:39 +0100

    Add new fields verification to userSignUp API

--- routes/user.js
+++ routes/user.js
@@ -8,17 +8,16 @@ const userController = require('../controllers/user');
 // @access Public
 
 router.post(
-	'/signup',
-	[
-		check('firstname', 'Firstname is required').not().isEmpty(),
-		check('lastname', 'Lastname is required').not().isEmpty(),
-		check('email', 'Please include a valid email address').isEmail(),
-		check(
-			'password',
-			'Please enter a password with 5 or more characters',
-		).isLength({ min: 5 }),
-	],
-	userController.signunp,
+  '/signup',
+  [
+    check('firstname', 'Firstname is required').not().isEmpty(),
+    check('lastname', 'Lastname is required').not().isEmpty(),
+    check('email', 'Please include a valid email address').isEmail(),
+    check('password', 'Please enter a password with 5 or more characters').isLength({ min: 5 }),
+    check('tags', 'At least one tag is required').not().isEmpty(),
+    check('state', 'State is required').not().isEmpty(),
+  ],
+  userController.signunp,
 );
 
 module.exports = router;
